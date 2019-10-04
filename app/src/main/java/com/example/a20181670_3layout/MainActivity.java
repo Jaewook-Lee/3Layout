14c6cc46667a2eea59dbcb5c143c76b42aba025b
Jaewook Lee <swljw@kookmin.ac.kr>
2019-10-04 21:44:28 +0900

    Git Initialization.

---
+++ app/src/main/java/com/example/a20181670_3layout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a20181670_3layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
