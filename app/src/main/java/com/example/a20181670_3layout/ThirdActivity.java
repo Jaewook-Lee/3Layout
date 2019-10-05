94278866a3a2cdcadc30c3cbfcc0fa7c51a49c2f
Jaewook Lee <swljw@kookmin.ac.kr>
2019-10-05 22:32:29 +0900

    Just for testing third layout.

---
+++ app/src/main/java/com/example/a20181670_3layout/ThirdActivity.java
@@ -0,0 +1,13 @@
+package com.example.a20181670_3layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThirdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout);
+    }
+}
