5b591cc358e59a079ccffcc870d9faf9a477c27c
Jaewook Lee <swljw@kookmin.ac.kr>
2019-10-06 00:01:04 +0900

    Made a simple clock.

--- app/src/main/java/com/example/a20181670_3layout/ThirdActivity.java
+++ app/src/main/java/com/example/a20181670_3layout/ThirdActivity.java
@@ -1,13 +1,115 @@
 package com.example.a20181670_3layout;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class ThirdActivity extends Activity {
+    TextView Date, Gre;
+    UsedAsync asyncTask;
+    ProgressHandler handler;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    String time;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third_layout);
+
+        Date = (TextView) findViewById(R.id.Date);
+        Gre = (TextView) findViewById(R.id.Gre);
+
+        handler = new ProgressHandler();
+
+        runtime();
+
+    }
+
+    public void runtime() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        time = sdf.format(new Date(System.currentTimeMillis()));
+
+                        Message message = handler.obtainMessage();
+                        handler.sendMessage(message);
+
+                        Thread.sleep(1000);
+                    }catch (InterruptedException ex) {}
+                }
+            }
+        });
+        thread.start();
+
+        asyncTask = new UsedAsync();
+        asyncTask.execute();
+    }
+
+    class UsedAsync extends AsyncTask<Integer, Integer, Integer> {
+        Calendar cal;
+        String timeGre;
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            while (isCancelled() == false) {
+                cal = new GregorianCalendar();
+                timeGre = String.format("%d/%d/%d %d:%d:%d", cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1,
+                        cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE),
+                        cal.get(Calendar.SECOND));
+                publishProgress();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            cal = new GregorianCalendar();
+            timeGre = String.format("%d/%d/%d %d:%d:%d", cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1,
+                    cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE),
+                    cal.get(Calendar.SECOND));
+            Gre.setText(timeGre);
+
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer){
+            super.onPostExecute(integer);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            Gre.setText(timeGre);
+
+            super.onProgressUpdate(values);
+        }
+    }
+
+    class ProgressHandler extends Handler {
+
+        @Override
+        public void handleMessage(Message msg) {
+            Date.setText(time);
+        }
     }
 }
