8449ee554936781b68d4e8bab6d1216eef2b05de
Jaewook Lee <swljw@kookmin.ac.kr>
2019-10-05 17:08:09 +0900

    Just for testing second layout.

---
+++ app/src/main/java/com/example/a20181670_3layout/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.a20181670_3layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
