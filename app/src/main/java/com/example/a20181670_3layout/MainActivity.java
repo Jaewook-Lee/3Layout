b984f2ca9382802012d006b964ee71cae972293d
Jaewook Lee <swljw@kookmin.ac.kr>
2019-10-07 23:53:15 +0900

    Toasting message if login fail.

--- app/src/main/java/com/example/a20181670_3layout/MainActivity.java
+++ app/src/main/java/com/example/a20181670_3layout/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.a20181670_3layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSecondActivity, btnThirdActivity;
+        btnSecondActivity = (Button) findViewById(R.id.btnLogin);
+        btnThirdActivity = (Button) findViewById(R.id.btnSignUp);
+
+        btnSecondActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(getApplicationContext(), ThirdActivity.class);
+
+                if (false) {    // statement will be fixed later.
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getBaseContext(), "아이디 또는 비밀번호가 틀렸습니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnThirdActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
