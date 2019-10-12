f666fdeccd1f54229910e534a52c2b10cda18439
Jaewook Lee <swljw@kookmin.ac.kr>
2019-10-12 15:42:58 +0900

    Set minimum password's length to 6.

--- app/src/main/java/com/example/a20181670_3layout/SecondActivity.java
+++ app/src/main/java/com/example/a20181670_3layout/SecondActivity.java
@@ -1,6 +1,5 @@
 package com.example.a20181670_3layout;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -48,6 +47,14 @@ public class SecondActivity extends AppCompatActivity {
                     if (id.equals("") || pw.equals("") || name.equals("") || phone_num.equals("") || addr.equals("")) {
                         Toast.makeText(getApplicationContext(), "모든 정보를 작성하셔야 합니다.", Toast.LENGTH_SHORT).show();
                     }
+                    else if (isPWImpossible(pw)) {
+                        Toast.makeText(getApplicationContext(), "허용하지 않는 특수 키를 사용하셨습니다.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    else if (pw.length() < 6) {
+                        Toast.makeText(getApplicationContext(), "비밀번호는 6자 이상이어야 합니다.",
+                                Toast.LENGTH_SHORT).show();
+                    }
                     else if (!pw.equals(pw_check)) {
                         Toast.makeText(getApplicationContext(), "비밀번호가 일치하지 않습니다." +
                                 " 다시 확인해주세요.", Toast.LENGTH_SHORT).show();
@@ -56,7 +63,7 @@ public class SecondActivity extends AppCompatActivity {
                         String[] userInfo = {id, pw, name, phone_num, addr};
                         UserData.writeToFile(SecondActivity.this, userInfo);
                         Toast.makeText(getApplicationContext(), "회원가입이 완료됐습니다." +
-                                "다시 로그인해주세요.", Toast.LENGTH_LONG).show();
+                                "앱을 다시 실행해주세요.", Toast.LENGTH_LONG).show();
                         finish();
                     }
                 }
@@ -65,5 +72,17 @@ public class SecondActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    boolean isPWImpossible(String password) {
+        final char[] impossible_chars = {'~', '`', '!', '\'', '\"'};
+        for (int i=0; i<password.length(); i++) {
+            for (int j=0; j<impossible_chars.length; j++) {
+                if (password.charAt(i) == impossible_chars[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
